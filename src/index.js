18c946175c3aa8edc6c912fed41f3de22b140267
Shelton Farias <sheltonm10@outlook.com>
2023-08-28 16:59:42 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
+const { query } = require('./database');
 var app = express();
 
 app.use(express.json());
 
-app.get('/', function(req, res) {
-  return res.send({ message: 'Hello world! '});
+app.get('/', async function(req, res) {
+  const contacts = await listContacts();
+
+  return res.json(contacts);
 });
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+function listContacts() {
+ return query('SELECT * FROM contatos;');
+}
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000/'));
