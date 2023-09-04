d40886f5ee4a4ae77ab8b199ecd9e498bc82a172
Shelton Farias <sheltonm10@outlook.com>
2023-09-04 15:48:08 -0300

    insert functions deletecontacts, insertcontacts

--- src/index.js
+++ src/index.js
@@ -10,8 +10,19 @@ app.get('/contacts', async function(req, res) {
   return res.json(contacts);
 });
 
-app.put('/contatos/:id')
-app.drop('/contatos/:id')
+app.put('/contatos/:id', async function(req, res) {
+  const contacts = await updatecontacts();
+
+  return res.json(contacts);
+});
+
+
+app.drop('/contatos/:id', async function(req, res) {
+  const contacts = await deletecontacts();
+
+  return res.json(contacts);
+});
+
 
 function listContacts() {
  return query('SELECT * FROM contatos;');
@@ -19,6 +30,14 @@ function listContacts() {
 
 function updatecontacts () {
   return query('UPDATE contatos where id = 2')
+}
+
+function deletecontacts() {
+  return query('DELETE FROM contatos;')
+}
+
+function insertcontacts() {
+  return query('INSERT INTO contatos')
 }
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000/'));
