b1c3bfd1abb58fee710afd69e609644469ccdd32
Mirton Marques <m-marques-gomes@hotmail.com>
2023-09-06 16:43:10 -0300

    alter function

--- src/index.js
+++ src/index.js
@@ -4,40 +4,23 @@ var app = express();
 
 app.use(express.json());
 
-app.get('/contacts', async function(req, res) {
+app.get('/', async function(req, res) {
   const contacts = await listContacts();
 
   return res.json(contacts);
 });
 
-app.put('/contatos/:id', async function(req, res) {
-  const contacts = await updatecontacts();
-
-  return res.json(contacts);
-});
-
-
-app.drop('/contatos/:id', async function(req, res) {
-  const contacts = await deletecontacts();
-
-  return res.json(contacts);
-});
-
-
 function listContacts() {
  return query('SELECT * FROM contatos;');
+} 
+
+
+function createcontacts () {
+  return query('CREATE')
 }
 
 function updatecontacts () {
-  return query('UPDATE contatos where id = 2')
-}
-
-function deletecontacts() {
-  return query('DELETE FROM contatos;')
-}
-
-function insertcontacts() {
-  return query('INSERT INTO contatos')
+  return query('UPDATE contatos')
 }
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000/'));
