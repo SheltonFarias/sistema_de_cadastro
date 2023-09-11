153bd7ff7cb67dbe0d8d8fff107d8f46716b22ae
Mirton Marques <m-marques-gomes@hotmail.com>
2023-09-11 16:40:59 -0300

    oi

--- src/index.js
+++ src/index.js
@@ -118,6 +118,18 @@ app.get('/categorias', async function(req, res) {
   return res.json(contatos);
 });
 
+ 
+//Rota Delete
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
 
 
 
