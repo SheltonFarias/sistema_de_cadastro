4859e4885e6951176bbf2e5015f3d87f3f4ab521
Shelton Farias <sheltonm10@outlook.com>
2023-09-22 16:28:48 -0300

    correction deletecategorias

--- src/controllers/categorias/delete.js
+++ src/controllers/categorias/delete.js
@@ -1,4 +1,4 @@
-const { query } = require('../../database')
+//const { query } = require('../../database')
 const {  deleteCategorias } = require('../../repositories/categorias-repository.js')
 
 async function remove(req, res) {
@@ -8,8 +8,9 @@ async function remove(req, res) {
 		return res.status(400).json({ error: 'Nome, email e telefone são obrigatórios.' })
 	}
 	try {
-		const result = await query(deleteCategorias)
-		res.status(200).json(result[0])
+		const result = await deleteCategorias(id)
+		res.status(200).json(result)
+		console.log(result)
 	} catch (error) {
 		console.error('Erro ao excluir registro:', error)
 		res.status(500).json({ error: 'Erro ao excluir registro' })
