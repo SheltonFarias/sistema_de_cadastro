52b6f44111faa2a42928fedd99ff19048c9c95ae
Shelton Farias <sheltonm10@outlook.com>
2023-10-11 17:01:22 -0300

    retorno do GET para 'result-contacts'

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,8 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-	origin: '*'
+	origin: '*',
+	methods:'GET, PUT, POST, DELETE'
 }))
 
 app.use(routes)
--- frontend/contacts/script.js
+++ frontend/contacts/script.js
@@ -114,7 +114,22 @@ function editarContato(icon) {
   let email = row.getElementsByTagName("td")[1].textContent;
   let telefone = row.getElementsByTagName("td")[2].textContent;
   let categoria = row.getElementsByTagName("td")[3].textContent;
-  
+    
+  fetch('http://localhost:3000/contatos', {
+    method: 'PUT',
+    headers:{
+      'content-type': 'application/json',
+    },
+    body: {
+      name: nome,
+      email:email,
+      phone: telefone,
+      categoria: categoria 
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
   // Passa a edição para o formulario register-contacts
   document.getElementById("nome").value = nome;
   document.getElementById("email").value = email;
@@ -153,4 +168,47 @@ function editarContato(icon) {
   adicionarBotao.onclick = adicionarContato;
 }
 
+// Função para preencher a tabela com os dados da consulta GET
+function preencherTabelaComContatos(contatos) {
+  let table = document.getElementById("result-contacts").getElementsByTagName('tbody')[0];
+  // Limpe a tabela antes de preencher com os novos dados
+  table.innerHTML = "";
+
+  contatos.forEach(contato => {
+    let newRow = table.insertRow(table.rows.length);
+
+    let cell1 = newRow.insertCell(0);
+    cell1.textContent = contato.name;
+
+    let cell2 = newRow.insertCell(1);
+    cell2.textContent = contato.email;
+
+    let cell3 = newRow.insertCell(2);
+    cell3.textContent = contato.phone;
+
+    let cell4 = newRow.insertCell(3);
+    cell4.textContent = contato.categoria;
+
+    let cell5 = newRow.insertCell(4);
+    cell5.innerHTML = '<i class="fa fa-trash" onclick="excluirContato(this)"></i>' +
+      ' <i class="fa fa-pencil" onclick="editarContato(this)"></i>';
+  });
+}
+
+// Função para realizar a consulta GET e preencher a tabela
+function atualizarTabelaComContatos() {
+  fetch('http://localhost:3000/contatos', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      preencherTabelaComContatos(data); // Preenche a tabela com os dados recebidos
+    });
+}
+
+// Chame a função para preencher a tabela quando a página for carregada
+window.addEventListener('load', atualizarTabelaComContatos);
 
